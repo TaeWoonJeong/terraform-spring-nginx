a5bc1cda4cd7894db1df3d4e269916074c247cf4
정태운 <50254500+TaeWoonJeong@users.noreply.github.com>
2023-08-25 20:15:48 +0900

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -3,6 +3,6 @@ cd /home/ubuntu/spring
 pid = `ps -ef | grep spring-nginx | grep -v grep | awk '{print $2}'`
 if [ $pid ]
   then
-    kill pid
+    kill $pid
 fi
 nohup java -jar /home/ubuntu/spring/build/libs/spring-nginx-0.0.1-SNAPSHOT.jar 1>> /home/ubuntu/spring.log 2>> /home/ubuntu/spring-error.log &
